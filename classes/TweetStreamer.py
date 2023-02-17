6020a807855c70d8454bf4a67e81832547e2c894
Toki <gorjan4ilijevski@gmail.com>
2023-02-17 17:12:33 +0100

    TweetStreamer class created

---
+++ classes/TweetStreamer.py
@@ -0,0 +1,64 @@
+import tweepy
+from tweepy import StreamingClient, StreamRule
+import os
+from dotenv import load_dotenv
+from helpers.tweepyClient import getTweepyClient
+
+load_dotenv()
+
+BEARER_TOKEN = os.getenv("BEARER_TOKEN")
+CHAT_ID = os.getenv("TELEGRAM_NARRATIVE_TRADERS_CHAT_ID")
+client = getTweepyClient()
+
+
+class TweetPrinterV2(tweepy.StreamingClient):
+    def on_tweet(self, tweet):
+        tweet = client.get_tweet(tweet.id)
+        print(tweet)
+        # print(f"{tweet.id} {tweet.created_at} ({tweet.author_id}): {tweet.text}")
+        # print("tweet Object: ", tweet)
+        # print("-" * 50)
+
+    def on_connect(self):
+        print("connected")
+
+
+class TweetStreamer:
+    def __init__(self, list) -> None:
+        self.accountsToTrack = list
+        self.tweetPrinter = TweetPrinterV2(BEARER_TOKEN)
+
+    # clean rules
+    def cleanRules(self):
+        ruleIds = []
+        rules = self.tweetPrinter.get_rules()
+        rulesArr = rules.data
+        if rulesArr != None:
+            for rule in rulesArr:
+                print(f"rule marked to delete: {rule.id} - {rule.value}")
+                ruleIds.append(rule.id)
+
+            if len(ruleIds) > 0:
+                self.tweetPrinter.delete_rules(ruleIds)
+                self.tweetPrinter = TweetPrinterV2(BEARER_TOKEN)
+            else:
+                print("no rules to delete")
+
+    def ruleString(self):
+        index = 0
+        ruleString = ""
+        for account in self.accountsToTrack:
+            ruleString += "from: " + account + " "
+            if index != len(self.accountsToTrack) - 1:
+                ruleString += "OR "
+            index += 1
+
+        print(ruleString)
+        return ruleString
+
+    # Start printer
+    def startStreaming(self):
+        self.cleanRules()
+        rules = StreamRule(self.ruleString())
+        self.tweetPrinter.add_rules(rules)
+        self.tweetPrinter.filter(expansions="author_id")
---
+++ scripts/tweetReads.py
@@ -0,0 +1,7 @@
+from TweetStreamer import TweetStreamer
+
+listAccounts = ["1449328468227932163", "2966287497"]
+
+streamer = TweetStreamer(listAccounts)
+
+streamer.startStreaming()
